8b9bc12a34fd8cec856a9a13d78e5f9580cb1f1c
galfin24 <galfin.muzani@student.uinsgd.ac.id>
2014-09-26 09:46:14 +0700

    tugas1galfinsigit

---
+++ HitungTahunKabisat.java
@@ -0,0 +1,11 @@
+import java.util.*;
+	class HitungTahunKabisat {
+	public static void main(String[] args) {
+		String thk="Tahun Kabisat",thbk="bukan tahun kabisat";
+		int th;
+		Scanner input=new Scanner(System.in);
+	System.out.println("Hitung Tahun Kabisat");
+	System.out.println("--------------------");
+	System.out.print("Masukan Tahun (1900-2020): ");
+	th=input.nextInt();
+		
